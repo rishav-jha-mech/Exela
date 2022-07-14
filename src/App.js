58992f907397431b285f222a428444a3883695b8
rishav-jha-mech <rishav.rj74@gmail.com>
2022-07-14 19:37:35 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import HomePage from './Pages/Home/index'
+// import NewsPage from './Components/Pages/NewsPage'
+// import Error404 from "./Components/Error404";
+// import ComingSoon from "./Components/ComingSoon";
+import './App.css'
 
 function App() {
+  console.clear()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+          <Switch>
+          <Route path="/" component={HomePage} exact/>
+          {/* <Route path="/" component={NewsPage} /> */}
+          {/* <Route path="/" component={ComingSoon} /> */}
+          {/* <Route path="/blogs" component={ComingSoon} /> */}
+          {/* <Route component={Error404} /> */}
+          </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
