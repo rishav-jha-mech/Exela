905627aae3713769c2386a56fd5912146d3fd62c
rishav-jha-mech <rishav.rj74@gmail.com>
2022-07-14 21:57:22 +0530

    Bill list showing

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,30 +1,91 @@
-import React from 'react'
+import {Link} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
 
-const Home = () => {
-  
-  return (
-    <div className='fullmh container'>
-      <h1 className='my-3'>Exela Electricity Bill</h1>
-      <hr />
-      {/* ThE TABLE */}
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Sl. no</th>
-            <th scope="col">Month</th>
-            <th scope="col">Year</th>
-            <th scope="col">Bill date</th>
-            <th scope="col">Paid date</th>
-            <th scope="col">Unit Consumed</th>
-            <th scope="col">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
+const Home = ({navigation}) => {
 
-        </tbody>
-      </table>
-    </div>
-  )
+	const [data, setData] = useState([])
+	const [Loading, setLoading] = useState(true)
+	const [Error, setError] = useState(false)
+
+	useEffect(() => {
+		fetch('http://127.0.0.1:4000', {
+			method: 'GET',
+		}).then(res => res.json())
+			.then(res => {
+				console.log(res);
+				setData(res.resultArray)
+				setLoading(false)
+			}).catch(err => {
+				setError(true);
+				Swal.fire(
+					'Error',
+					`${err}`,
+					'error'
+				)
+			})
+	}, [Loading, Error])
+
+	const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',]
+	const ToDate = (param) => {
+		try {
+			const date = new Date(param)
+			return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+		}
+		catch {
+			return 'Error'
+		}
+	}
+
+	return (
+		<div className='fullmh container'>
+			<h1 className='my-4'>Exela Electricity Bill ({data.length})</h1>
+			<hr />
+			{
+				Error ? <h1 className='my-5 text-center'>Error Occured while loading bills</h1>
+					: Loading ?
+						<div className='my-5 py-5 d-flex justify-content-center minh250 w-100'>
+							<div className="spinner-border" role="status">
+								<span className="visually-hidden d-block">Loading...</span>
+							</div>
+						</div>
+						:
+						<div className='table-responsive'>
+							<table className="table">
+								<thead>
+									<tr>
+										<th scope="col">Sl. no</th>
+										<th scope="col">Month</th>
+										<th scope="col">Year</th>
+										<th scope="col">Bill date</th>
+										<th scope="col">Paid date</th>
+										<th scope="col">Unit Consumed</th>
+										<th scope="col">Amount</th>
+									</tr>
+								</thead>
+								<tbody>
+									{
+										data.map((item, index) => {
+											let bill_date = new Date(item.bill_date)
+											return (
+												<tr>
+													<th scope="row">{index + 1}</th>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {MONTHS[bill_date.getMonth()]}</Link></td>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {bill_date.getFullYear()}</Link></td>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {ToDate(bill_date)}</Link></td>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {ToDate(item.paid_date)}</Link></td>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {item.units}</Link></td>
+													<td> <Link style={{textDecoration: 'none',color: 'black'}} to={`bill/${item._id}`}> {item.amount}</Link></td>
+												</tr>
+											);
+										})
+									}
+								</tbody>
+							</table>
+						</div>
+			}
+		</div>
+	)
 }
 
 
